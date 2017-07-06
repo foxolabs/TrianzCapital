439a7ff81ce649e52bb00e996d897962a7fc06ab
Sanjeev <samdraiz@gmail.com>
2017-07-06 14:16:46 +0530

    add difference draft

--- src/components/Difference.vue
+++ src/components/Difference.vue
@@ -1,30 +1,104 @@
 <template>
   <div class="difference">
-    <div class="hero is-large top"></div>
-    <h1>{{ msg }}</h1>
+    <div class="hero is-large top has-text-centered">
+      <div class="hero-body">
+        <div class="container">
+          <div class="title is-4">We are Pioneers in GLOBAL Value investing</div>
+          <br>
+          <p class="subtitle is-6"> We are a nimble boutique hedge fund with streamlined infromation flow which enhances our timely decision making. We consistently strive to be the best, not the biggest. </p>
+        </div>
+      </div>
+    </div>
+    <div class="hero is-medium has-text-centered grey">
+      <div class="hero-body">
+        <div class="container">
+          <h6 class="title is-4"> Trianz Capital HEDGE FUND apart</h6>
+          <h6 class="subtilte">We invest in US, Europe and Emerging markets-India, China and Brazil which helps to diversify risks. Emerging-markter growth has generally outpaced that of developed markets in the last 15 years and this is forecasted to contimue. Despite their growth significance in the global investing arena, we belive emerging-market equities have been under-allocated within well-diversified portfolios 100% employee owned firm and avendus strategies are unhindered by sales quotas, allocation quota or any outside influence than many impede the firm's pursuit of investment management excellence.</h6>
+        </div>
+      </div>
+    </div>
+    <div class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-one-third">IMG</div>
+            <div class="column">
+              <div class="hero">
+                <div class="hero-body">
+                  <p class="title is-5">TEAM APPROACH</p>
+                  <p class="subtilte is-8">Focus on longterm results through a team of dedicated investment professionals who engage in rigorous debates that are encourage diverse prespectives</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="column">
+              <div class="hero">
+                <div class="hero-body">
+                  <p class="title is-5">GAIN EXPOSURE TO GLOBAL COMPANIES DOING BUSINESS IN EMERGING MARKETS</p>
+                  <p class="subtilte is-8">Trianz investments include companies with operations in emerging markets. We provide you diversification and enhanced return potential by investing in different markets</p>
+                </div>
+              </div>
+            </div>
+            <div class="column is-one-third">IMG</div>
+          </div>  
+          <div class="columns">
+            <div class="column is-one-third">IMG</div>
+            <div class="column">
+              <div class="hero">
+                <div class="hero-body">
+                  <p class="title is-5">MARKET CAPTIALIZATION COVERAGE</p>
+                  <p class="subtilte is-8">The flexibility to invest in any company that we believe is solid long-term investment regardless of size</p>
+                </div>
+              </div>
+            </div>
+          </div>  
+        </div>
+      </div>
+    </div>
+    <div class="hero has-text-centered grey">
+      <div class="hero-body">
+        <div class="container">
+          <h6 class="title is-4"> INDEPENDENT POINT OF VIEW </h6>
+          <h6 class="subtilte">Being a 100% employee-owned firm allows to sharply focus on helping clients pursue thier long-term financial goals-free of outside influences</h6>
+        </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="container">
+          <h6 class="title is-4"> WHY TREAT YOU ANY DIFFERNTLY THAN OUR EMPLOYEES </h6>
+          <h6 class="subtilte">For all the funds you invest with us, we charge only 2% of the profits and 0.75% as our managing fees. This is same as our employees pay.</h6>
+        </div>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
-export default {
-  name: 'difference',
-  data () {
-    return {
-      msg: 'This is difference'
+  export default {
+    name: 'difference',
+    data () {
+      return {
+        msg: 'This is difference'
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-  color: black;
-}
-.top {
-  height: 30em;
-  background: grey;
-}
+  h1, h2 {
+    font-weight: normal;
+    color: black;
+  }
+  .top {
+    height: 30em;
+    background: grey;
+  }
+  .grey {
+    background-color: #f5f5f5;
+  }
 
 </style>
