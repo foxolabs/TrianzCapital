0353b02243b974859d9a119dcbeb34751260c5de
Sanjeev <samdraiz@gmail.com>
2017-07-07 14:56:58 +0530

    add process draft page

--- src/components/Pap.vue
+++ src/components/Pap.vue
@@ -1,31 +1,229 @@
 <template>
   <div class="pap">
-  <div class="hero is-large top"></div>
-    <h1>{{ msg }}</h1>
+    <div class="hero is-large top has-text-centered">
+      <div class="hero-body">
+        <div class="container">
+          <div class="title is-4">Philosophy and Success</div>
+          <br>
+          <p class="subtitle is-6"> Driven by a singular goal to help investors pursue their long-term investments objectives, Trianz Capital has scoured the globe for attractively price companies since the firm's founding in Asia in 1964. </p>
+        </div>
+      </div>
+    </div>
+
+          <div class="columns has-text-centered">
+            <div class="column lfirst lgreen">
+              <div class=" title is-4 lwhite">A Pioneer in Global Value Investing</div>
+              <br>
+              <div class="subtitle is-6 lwhite">Driven by a singualr goal to help investors pusue their long-term investment objectives, Trianz Capotial has scoured the globe for attractively proiced companies since the firm founding in Asia in 1964. Trianz Caoptial was amoung the first investment firms to invest globally using a value approach</div>
+            </div>
+            <div class="column lfirst lcyan">
+              <div class="title is-4 lwhite">Inspired by the pursuit of Value-Driven Growth Potential</div>
+              <br>
+              <div class="subtitle is-6 lwhite">Finding value for client inspires Trianz Caoptal just as much today as did twhen we started more than 50years afa. After navigating five decades of market ebbs and flows, we continue to believe true value investing is the only way toseek meaningful, longterm growth potential for our clients.</div>
+            </div>
+            <div class="column lfirst lviolet">
+              <div class="title is-4 lwhite">Invested in Our Strategies Aongside Our Clients</div>
+              <br>
+              <div class="subtitle is-6 lwhite">We have a long-established culture of investing alongside of out clients. Our Co-investment Guidelines ensure that our leadership's financial interests are aligned with those of our clients by requiring that all members of firm's various Investment Committees maintain meaniful personal investments in Avendus startegies, especially in those they co-manage.</div>
+            </div>
+          </div>
+        
+    <div class="hero has-text-centered">
+      <div class="hero-body">
+        <div class="container">
+          <p class="subtitle is-4">As of June 30, 2016, Trianz empoyees and parterns had 32$ million invested in various Trianz strategies. </p>
+        </div>
+      </div>
+    </div>
+    <div class="hero is-medium has-text-centered">
+      <div class="hero-body">
+        <div class="container">
+          <p class="subtitle"><b>OPPORTUNITIES ABOUND GLOBALLY --</b> With over half of the world's equity value located outside the United States, oppotunities abound. </p>
+          <br>
+          <p class="subtitle"><b>BRODERLESS INVESTING -- </b> A global approach that searches for individual companies around the world, including the U.S. and emerging markets. </p>
+        </div>
+      </div>
+    </div>
+    <div class="hero has-text-centered">
+      <div class="hero-body">
+        <div class="container">
+          <p class="subtitle">We believe that displined value investing, built on a research-driven foundation, can deliver investment success over time. Promising oppportunity can be found amoung securities that are most deeply undervalued relative to their future earingins potential. </p>
+        </div>
+      </div>
+    </div>
+    <div class="hero has-text-centered">
+      <div class="hero-body">
+        <div class="container">
+          <p class="title is-4 rTitle">Team Approach</p>
+        </div>
+      </div>
+    </div>
+    <div class="columns has-text-centered">
+      <div class="column is-one-third">
+        <div class="section rTitle">EXPANED OPPORTUNITY SET</div>
+      </div>
+      <div class="column is-two-third">
+        <div class="box">As the world becomes more interconnceted, international and emering market companies offer a wider range of opportunities for value investors.
+        </div>
+      </div>
+    </div>
+    <div class="columns has-text-centered">
+      <div class="column is-one-third">
+        <div class="section rTitle">BROAD OPPORTUNITY SET</div>
+      </div>
+      <div class="column is-two-third">
+        <div class="box">The startegy can invest in companies across the market capitalization spectrim - small, medium and large - in pursuit of potential opportunities.
+        </div>
+      </div>
+    </div>
+    <div class="columns has-text-centered">
+      <div class="column is-one-third">
+        <div class="section rTitle">BIG POND</div>
+      </div>
+      <div class="column is-two-third">
+        <div class="box"> International small cap stocks, with approximatelty 7,600 companies offer nearly three times as many investment opportunities as leage and mid-sized internation companies.
+        </div>
+      </div>
+    </div>
+    <div class="columns has-text-centered">
+      <div class="column is-one-third">
+        <div class="section rTitle">FEW FISHERMAN</div>
+      </div>
+      <div class="column is-two-third">
+        <div class="box">But they tend to be far less researched by the investment community, which creates opportunity to find undervalued companies other may miss.
+        </div>
+      </div>
+    </div>
+    <div class="columns has-text-centered">
+      <div class="column is-one-third">
+        <div class="section rTitle">PATIENCE</div>
+      </div>
+      <div class="column is-two-third">
+        <div class="box">Our long-term focus means we are willing to look beyond short-term market noise to seek undervalued opportunites.
+        </div>
+      </div>
+    </div>
+    <div class="hero dark">
+      <div class="hero-body">
+        <div class="container">
+          <b>Deep Value equity is singular focus of Trianz investment startegy. We employ an active startegy that invests primarily in US, Emerging Markets - India, China, Brazil equities with market captializations in the range of the relevant benchmark index.</b>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container  has-text-centered ">
+        <div class="title rTitle is-4">Deep Value Research</div>
+        <br>
+        <div class="subtitle is-6">
+          We have a structured process to buidling and managing the funds. Our research team works to identify companies that are prices at a substantial discount to what we consider to be thier underlying business value - what a rational investor would pay to own the entire business - and then we patiently wait for the gap between price and value to narrow.
+        </div>
+        <div class="subtitle is-6">
+          We invest alongside our investors: When selecting investments for our funds, one of the traits we look is a hihg level of stock ownership by management. We believe that our own stock ownership along with the investors for whom we invest is a best way to show - WE LIVE, BREATH and SLEEP THIS STUFF ... Eating our own food... to make sure interests are correctly aligned with ours.
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="subtitle is-6">
+          <p> To Capture promising investment opportunities, our team engages in a systematic approach consisting of:</p>
+        </div>
+        <div class="subtitle is-6">
+          <ul>
+            <li>
+              Insightful Research: geared towards identifying securities with lower investor expectations that are trading at a substantial discount to their underlying business value
+            </li>
+            <li>
+              Thoughtful Stock Selection: faciliated by a proprietory ranking system that identifies which secuirities can add the most risk-adjusted value to the portfolio
+            </li>
+            <li>
+              A Rigorous Sell Discipline: designed to capture upside potential and prune portfolio laggards
+            </li>
+          </ul> 
+        </div>
+      </div>
+    </div>
+    <div class="columns has-text-centered">
+      <div class="column lyellow">
+        <div class="title is-4">
+          Why to invest in International and Emerging markets and not just US markets
+        </div>
+        <div class="content">
+          There are 2 reasons to have exposure to internation markets: Diversification or risk balancing and return potential. It helps investors have a balances economic protfolio, benefit from long-term shift in US policies and take advantage of shifts in economy in other countries. We help investors balance the risks from emerging markets delivering better returns.
+        </div>
+      </div>
+      <div class="column lorange">
+        <div class="title is-4">
+          Capitalize
+        </div>
+        <div class="content">
+          The economic exopansion in Asia and huge population provides a great opportunity  for investors. IMF forcasts Asia Pacfic willl remain global economic growth leaders in medium term. Allows opportunity to potentiallly capitalize on short-term market volatility. 
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'pap',
-  data () {
-    return {
-      msg: 'This is pap'
+  export default {
+    name: 'pap',
+    data () {
+      return {
+        msg: 'This is pap'
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-  color: black;
-}
-.top {
-  height: 30em;
-  
-  background: grey;
-}
+  h1, h2 {
+    font-weight: normal;
+    color: black;
+  }
+  .top {
+    height: 30em;
 
+    background: grey;
+  }
+  .rTitle {
+    color: #c50404;
+  }
+  .is-one-third {
+    text-align: right;
+  }
+  .is-two-third {
+    text-align: justify;
+    padding: 2em;
+  }
+  .is-two-third .box {
+    background-color: #d7d7d7;
+  }
+  .dark {
+    background-color: #2d2d2d;
+    color: white;
+  }
+  ul {
+    list-style-type: disc;
+  }
+  .lfirst {
+    padding-top: 2em;
+  }
+  .lgreen {
+    background-color: #3cb878;
+  }
+  .lcyan {
+    background-color: #1cbbb4;
+  }
+  .lviolet {
+    background-color: #8781bd;
+  }
+  .lyellow {
+    background-color: #fff200;
+  }
+  .lorange {
+    background-color: #f7941d;
+  }
+  .lwhite {
+    color: white;
+  }
 </style>
